e735bb28105fdcdd8d1672f4cc6f46b2eda10fbf
Raymond <raymondtdev@gmail.com>
2025-03-30 22:01:03 +0200

    fix: contact form functionality

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const menu = document.getElementById("menu");
 
+function toggleMenu() {
+  menu.classList.toggle("active");
+}
 
+function displayAnswer(num) {
+  const questionAnswer = document.getElementById(`answer${num}`);
+  const verticalBar = document.getElementById(`bar${num}`);
 
+  questionAnswer.classList.toggle("display");
+  verticalBar.classList.toggle("display");
+}
